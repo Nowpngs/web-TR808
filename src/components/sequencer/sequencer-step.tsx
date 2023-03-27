7a9fc2bf654ecf679c7f74d879265f31b13976ca
Now <now.thongtangsai@gmail.com>
2023-03-27 20:54:55 +0700

    Adding the active color for the step sequence

--- src/components/sequencer/sequencer-step.tsx
+++ src/components/sequencer/sequencer-step.tsx
@@ -1,11 +1,19 @@
-export interface SequecerStepProps {}
+export interface SequecerStepProps {
+  active?: boolean;
+}
 
-export default function SequecerStep() {
+export default function SequecerStep(props: SequecerStepProps) {
   return (
     <div className="w-full h-full px-1.5 flex items-end">
       <div className="background-instrument-secondary rounded-xl h-full w-full flex flex-wrap justify-center">
         <div className="w-full pt-2.5 flex justify-center">
-          <div className="bg-white w-[15px] h-[15px] rounded-full">&nbsp;</div>
+          <div
+            className={`w-[15px] h-[15px] rounded-full ${
+              props.active ? "bg-blue" : "bg-white"
+            }`}
+          >
+            &nbsp;
+          </div>
         </div>
       </div>
     </div>
