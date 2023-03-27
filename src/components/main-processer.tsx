91a68c77274cf39a229d73ddd73da5f9106081e2
Now <now.thongtangsai@gmail.com>
2023-03-27 20:11:08 +0700

    Setting up the start stop button UI

--- src/components/main-processer.tsx
+++ src/components/main-processer.tsx
@@ -4,10 +4,10 @@ import MainSequencer from "./sequencer/main-sequencer";
 export default function MainProcesser() {
   return (
     <div className="background-instrument w-full h-full rounded-xl">
-      <div className="pt-10 px-16 pb-5 h-3/5">
+      <div className="pt-10 px-16 pb-5 h-3/4">
         <MainController />
       </div>
-      <div className="pb-10 px-16 h-2/5">
+      <div className="pb-10 px-16 h-1/4">
         <MainSequencer />
       </div>
     </div>
--- src/components/sequencer/main-sequencer.tsx
+++ src/components/sequencer/main-sequencer.tsx
@@ -1,5 +1,25 @@
 export interface MainSequencerProps {}
 
 export default function MainSequencer(props: MainSequencerProps) {
-  return <div className="bg-white w-full h-full">Main Sequencer</div>;
+  return (
+    <div className="background-instrument w-full h-full flex flex-wrap">
+      <div className="w-2/12 pr-2.5">
+        <div className="background-instrument-secondary h-full w-full rounded-xl flex justify-center p-5">
+          <button className="button-theme px-7 rounded-xl font-bold">
+            START
+          </button>
+        </div>
+      </div>
+      <div className="w-9/12">
+        <div className="background-instrument-secondary h-full w-full rounded-xl">
+          <button></button>
+        </div>
+      </div>
+      <div className="w-1/12 pl-2.5">
+        <div className="background-instrument-secondary h-full w-full rounded-xl">
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
 }
