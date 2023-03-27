2cbd48b1e841100433afea37b5f85b82ce6d8d3c
Now <now.thongtangsai@gmail.com>
2023-03-27 21:48:42 +0700

    Move the color calculation logic to common functions

--- src/components/sequencer/main-sequencer.tsx
+++ src/components/sequencer/main-sequencer.tsx
@@ -1,3 +1,4 @@
+import { calculateGradientColors } from "../../services/CommonFunctions";
 import SequecerStep from "./sequencer-step";
 
 export interface MainSequencerProps {
@@ -7,35 +8,6 @@ export interface MainSequencerProps {
 
 export default function MainSequencer(props: MainSequencerProps) {
   const steps = new Array(16).fill(false);
-
-  function calculateGradientColors(index: number): string {
-    const startColor = "hsl(16, 87.93%, 54.51%)";
-    const endColor = "hsl(46, 94%, 61%)";
-    const step = 11;
-
-    const hDiff =
-      parseInt(endColor.split(",")[0].substring(4)) -
-      parseInt(startColor.split(",")[0].substring(4));
-    const sDiff =
-      parseInt(endColor.split(",")[1]) - parseInt(startColor.split(",")[1]);
-    const lDiff =
-      parseInt(endColor.split(",")[2]) - parseInt(startColor.split(",")[2]);
-
-    const h =
-      (parseInt(startColor.split(",")[0].substring(4)) +
-        hDiff * (index / step)) %
-      360;
-    const s = Math.round(
-      parseInt(startColor.split(",")[1]) + sDiff * (index / step)
-    );
-    const l = Math.round(
-      parseInt(startColor.split(",")[2]) + lDiff * (index / step)
-    );
-    if (index > step) {
-      return `hsl(0, 0%, 100%)`;
-    }
-    return `hsl(${h}, ${s}%, ${l}%)`;
-  }
 
   return (
     <div className="background-instrument w-full h-full flex flex-wrap">
---
+++ src/services/CommonFunctions.ts
@@ -0,0 +1,27 @@
+export function calculateGradientColors(index: number): string {
+  const startColor = "hsl(16, 87.93%, 54.51%)";
+  const endColor = "hsl(46, 94%, 61%)";
+  const step = 11;
+  
+  const hDiff =
+    parseInt(endColor.split(",")[0].substring(4)) -
+    parseInt(startColor.split(",")[0].substring(4));
+  const sDiff =
+    parseInt(endColor.split(",")[1]) - parseInt(startColor.split(",")[1]);
+  const lDiff =
+    parseInt(endColor.split(",")[2]) - parseInt(startColor.split(",")[2]);
+
+  const h =
+    (parseInt(startColor.split(",")[0].substring(4)) + hDiff * (index / step)) %
+    360;
+  const s = Math.round(
+    parseInt(startColor.split(",")[1]) + sDiff * (index / step)
+  );
+  const l = Math.round(
+    parseInt(startColor.split(",")[2]) + lDiff * (index / step)
+  );
+  if (index > step) {
+    return `hsl(0, 0%, 100%)`;
+  }
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
