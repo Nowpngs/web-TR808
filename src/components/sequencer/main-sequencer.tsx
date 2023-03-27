7c156886a88d53e2c3af7229c1f5438245dc1617
Now <now.thongtangsai@gmail.com>
2023-03-27 21:31:51 +0700

    Create the step sequencer gradient color

--- src/components/sequencer/main-sequencer.tsx
+++ src/components/sequencer/main-sequencer.tsx
@@ -3,7 +3,37 @@ import SequecerStep from "./sequencer-step";
 export interface MainSequencerProps {}
 
 export default function MainSequencer(props: MainSequencerProps) {
-  const steps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  const steps = new Array(16).fill(false);
+
+  function calculateGradientColors(index: number): string {
+    const startColor = "hsl(16, 87.93%, 54.51%)";
+    const endColor = "hsl(46, 94%, 61%)";
+    const step = 11;
+
+    const hDiff =
+      parseInt(endColor.split(",")[0].substring(4)) -
+      parseInt(startColor.split(",")[0].substring(4));
+    const sDiff =
+      parseInt(endColor.split(",")[1]) - parseInt(startColor.split(",")[1]);
+    const lDiff =
+      parseInt(endColor.split(",")[2]) - parseInt(startColor.split(",")[2]);
+
+    const h =
+      (parseInt(startColor.split(",")[0].substring(4)) +
+        hDiff * (index / step)) %
+      360;
+    const s = Math.round(
+      parseInt(startColor.split(",")[1]) + sDiff * (index / step)
+    );
+    const l = Math.round(
+      parseInt(startColor.split(",")[2]) + lDiff * (index / step)
+    );
+    if (index > step) {
+      return `hsl(0, 0%, 100%)`;
+    }
+    return `hsl(${h}, ${s}%, ${l}%)`;
+  }
+
   return (
     <div className="background-instrument w-full h-full flex flex-wrap">
       <div className="w-1/12 pr-2.5">
@@ -17,7 +47,15 @@ export default function MainSequencer(props: MainSequencerProps) {
         <div className="h-full w-full">
           <div className="flex justify-between h-full w-full items-end">
             {steps.map((item, idx) => (
-              <SequecerStep />
+              <SequecerStep
+                key={idx}
+                active={item}
+                color={calculateGradientColors(idx)}
+                onClick={() => {
+                  // TODO: update the state
+                  console.log(idx);
+                }}
+              />
             ))}
           </div>
         </div>
--- src/components/sequencer/sequencer-step.tsx
+++ src/components/sequencer/sequencer-step.tsx
@@ -1,15 +1,25 @@
 export interface SequecerStepProps {
   active?: boolean;
+  color: string;
+  onClick: () => void;
 }
 
 export default function SequecerStep(props: SequecerStepProps) {
   return (
-    <div className="w-full h-full px-1.5 flex items-end">
-      <div className="background-instrument-secondary rounded-xl h-full w-full flex flex-wrap justify-center">
+    <div
+      className="w-full h-full px-1.5 flex items-end"
+      onClick={() => {
+        props.onClick();
+      }}
+    >
+      <div
+        className="rounded-xl h-full w-full flex flex-wrap justify-center"
+        style={{ backgroundColor: props.color }}
+      >
         <div className="w-full pt-2.5 flex justify-center">
           <div
             className={`w-[15px] h-[15px] rounded-full ${
-              props.active ? "bg-blue" : "bg-white"
+              props.active ? "bg-blue" : "bg-black"
             }`}
           >
             &nbsp;
